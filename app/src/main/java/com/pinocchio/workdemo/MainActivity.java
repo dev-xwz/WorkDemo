c35dd9f4b5eba1dd9d206956c35be7170583f8d8
dev_xwz <15162118020@163.com>
2024-02-03 17:22:13 +0800

    实现重叠个人头像图片

---
+++ app/src/main/java/com/pinocchio/workdemo/MainActivity.java
@@ -0,0 +1,50 @@
+package com.pinocchio.workdemo;
+
+import android.app.Activity;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.pinocchio.workdemo.adapter.OverlayAvatarAdapter;
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initOverlayAvatarRecycleView();
+    }
+
+    private void initOverlayAvatarRecycleView() {
+        RecyclerView overlayAvatarRecycleView = findViewById(R.id.rcv_overlay_avatar);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+//        linearLayoutManager.setStackFromEnd(false); //列表在底部开始展示
+//        linearLayoutManager.setReverseLayout(false); //列表反转
+        overlayAvatarRecycleView.setLayoutManager(linearLayoutManager);
+
+        OverlayAvatarAdapter overlayAvatarAdapter = new OverlayAvatarAdapter();
+        overlayAvatarRecycleView.setAdapter(overlayAvatarAdapter);
+
+        overlayAvatarRecycleView.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+                super.getItemOffsets(outRect, view, parent, state);
+                if (parent.getChildLayoutPosition(view) > 0) {
+                    outRect.left = dp2px( -30);
+                }
+            }
+        });
+    }
+
+    private int dp2px(int dps) {
+        return Math.round(getResources().getDisplayMetrics().density * dps);
+    }
+}
---
+++ app/src/main/java/com/pinocchio/workdemo/adapter/OverlayAvatarAdapter.java
@@ -0,0 +1,41 @@
+package com.pinocchio.workdemo.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.pinocchio.workdemo.R;
+
+public class OverlayAvatarAdapter extends RecyclerView.Adapter<OverlayAvatarAdapter.MyViewHolder> {
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_avatar, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.mIvAvatar.setImageResource(R.mipmap.ic_avatar);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 5;
+    }
+
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView mIvAvatar;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mIvAvatar = itemView.findViewById(R.id.iv_avatar);
+        }
+    }
+}
